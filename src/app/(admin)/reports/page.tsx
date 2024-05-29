746729043acf15e7ee7b7bbd65eee3d0d265f978
lasya.hemadri <srilasya.h@fervidsmart.com>
2024-05-29 13:01:20 +0530

    overall ui changes

---
+++ src/app/(admin)/reports/page.tsx
@@ -0,0 +1,12 @@
+import Reports from '@/components/Reports/Reports'
+import React from 'react'
+
+const ReportsPage = () => {
+  return (
+    <div>
+      <Reports/>
+    </div>
+  )
+}
+
+export default ReportsPage
